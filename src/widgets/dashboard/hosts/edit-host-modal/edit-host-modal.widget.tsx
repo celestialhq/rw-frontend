2c2cb8f5f810910ea7993a73643267566c19f5f3
kastov <yk@sent.com>
2026-06-18 16:49:26 +0300

    fix: host edit form

--- src/widgets/dashboard/hosts/edit-host-modal/edit-host-modal.widget.tsx
+++ src/widgets/dashboard/hosts/edit-host-modal/edit-host-modal.widget.tsx
@@ -1,25 +1,12 @@
-import { UpdateHostCommand } from '@remnawave/backend-contract'
-import { zodResolver } from 'mantine-form-zod-resolver'
-import { memo, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { PiListChecks } from 'react-icons/pi'
-import { useForm } from '@mantine/form'
 import { Drawer } from '@mantine/core'
+import { memo } from 'react'
 
-import {
-    QueryKeys,
-    useGetConfigProfiles,
-    useGetHostTags,
-    useGetInternalSquads,
-    useGetNodes,
-    useGetSubscriptionTemplates,
-    useUpdateHost
-} from '@shared/api/hooks'
 import { MODALS, useModalClose, useModalState } from '@entities/dashboard/modal-store'
 import { BaseOverlayHeader } from '@shared/ui/overlays/base-overlay-header'
-import { parseJsonField, stringifyJsonField } from '@shared/utils/misc'
-import { BaseHostForm } from '@shared/ui/forms/hosts/base-host-form'
-import { queryClient } from '@shared/api'
+
+import { EditHostModalContentWidget } from './edit-host-modal-content.widget'
 
 export const EditHostModalWidget = memo(() => {
     const { t } = useTranslation()
@@ -27,128 +14,10 @@ export const EditHostModalWidget = memo(() => {
     const { isOpen, internalState: host } = useModalState(MODALS.EDIT_HOST_MODAL)
     const close = useModalClose(MODALS.EDIT_HOST_MODAL)
 
-    const [advancedOpened, setAdvancedOpened] = useState(false)
-
-    const { data: configProfiles } = useGetConfigProfiles()
-    const { data: nodes } = useGetNodes()
-    const { data: templates } = useGetSubscriptionTemplates()
-    const { data: internalSquads } = useGetInternalSquads()
-    const { data: hostTags } = useGetHostTags()
-
-    const form = useForm<UpdateHostCommand.Request>({
-        name: 'edit-host-form',
-        mode: 'uncontrolled',
-        validateInputOnBlur: true,
-        onValuesChange: (values) => {
-            if (typeof values.vlessRouteId === 'string' && values.vlessRouteId === '') {
-                form.setFieldValue('vlessRouteId', null)
-            }
-        },
-        validate: zodResolver(UpdateHostCommand.RequestSchema.omit({ uuid: true }))
-    })
-
-    const handleClose = () => {
-        close()
-
-        setTimeout(() => {
-            form.reset()
-            form.resetDirty()
-            form.resetTouched()
-            setAdvancedOpened(false)
-        }, 200)
-    }
-
-    const { mutate: updateHost, isPending: isUpdateHostPending } = useUpdateHost({
-        mutationFns: {
-            onSuccess: async () => {
-                handleClose()
-                await queryClient.refetchQueries({
-                    queryKey: QueryKeys.hosts.getAllTags.queryKey
-                })
-            }
-        }
-    })
-
-    useEffect(() => {
-        if (host && configProfiles) {
-            form.initialize({
-                uuid: host.uuid,
-                remark: host.remark,
-                address: host.address,
-                port: host.port,
-                securityLayer: host.securityLayer,
-                isDisabled: !host.isDisabled,
-                sni: host.sni ?? undefined,
-                host: host.host ?? undefined,
-                path: host.path ?? undefined,
-                alpn: host.alpn ?? undefined,
-                fingerprint: host.fingerprint ?? undefined,
-                inbound: {
-                    configProfileUuid: host.inbound.configProfileUuid ?? '',
-                    configProfileInboundUuid: host.inbound.configProfileInboundUuid ?? ''
-                },
-                serverDescription: host.serverDescription ?? undefined,
-                xHttpExtraParams: stringifyJsonField(host.xHttpExtraParams),
-                muxParams: stringifyJsonField(host.muxParams),
-                sockoptParams: stringifyJsonField(host.sockoptParams),
-                finalMask: stringifyJsonField(host.finalMask),
-                tags: host.tags ?? undefined,
-                isHidden: host.isHidden,
-                overrideSniFromAddress: host.overrideSniFromAddress,
-                keepSniBlank: host.keepSniBlank,
-                vlessRouteId: host.vlessRouteId ?? undefined,
-                pinnedPeerCertSha256: host.pinnedPeerCertSha256 ?? undefined,
-                verifyPeerCertByName: host.verifyPeerCertByName ?? undefined,
-                shuffleHost: host.shuffleHost ?? undefined,
-                mihomoX25519: host.mihomoX25519 ?? undefined,
-                mihomoIpVersion: host.mihomoIpVersion ?? undefined,
-                nodes: host.nodes ?? undefined,
-                xrayJsonTemplateUuid: host.xrayJsonTemplateUuid ?? undefined,
-                excludedInternalSquads: host.excludedInternalSquads ?? undefined,
-                excludeFromSubscriptionTypes: host.excludeFromSubscriptionTypes ?? undefined
-            })
-        }
-    }, [host, configProfiles])
-
-    form.watch('inbound.configProfileInboundUuid', ({ value }) => {
-        const { inbound } = form.getValues()
-        if (!inbound?.configProfileUuid) {
-            return
-        }
-
-        const configProfile = configProfiles?.configProfiles.find(
-            (configProfile) => configProfile.uuid === inbound.configProfileUuid
-        )
-        if (configProfile) {
-            form.setFieldValue(
-                'port',
-                configProfile.inbounds.find((inbound) => inbound.uuid === value)?.port ?? undefined
-            )
-        }
-    })
-
-    const handleSubmit = form.onSubmit(async (values) => {
-        if (!host) {
-            return
-        }
-
-        updateHost({
-            variables: {
-                ...values,
-                isDisabled: !values.isDisabled,
-                uuid: host.uuid,
-                xHttpExtraParams: parseJsonField(values.xHttpExtraParams),
-                muxParams: parseJsonField(values.muxParams),
-                sockoptParams: parseJsonField(values.sockoptParams),
-                finalMask: parseJsonField(values.finalMask)
-            }
-        })
-    })
-
     return (
         <Drawer
             keepMounted={false}
-            onClose={handleClose}
+            onClose={close}
             opened={isOpen}
             overlayProps={{ backgroundOpacity: 0.6, blur: 0 }}
             padding="lg"
@@ -165,20 +34,7 @@ export const EditHostModalWidget = memo(() => {
                 />
             }
         >
-            {host && (
-                <BaseHostForm
-                    advancedOpened={advancedOpened}
-                    configProfiles={configProfiles?.configProfiles ?? []}
-                    form={form}
-                    handleSubmit={handleSubmit}
-                    hostTags={hostTags?.tags ?? []}
-                    internalSquads={internalSquads?.internalSquads ?? []}
-                    isSubmitting={isUpdateHostPending}
-                    nodes={nodes!}
-                    setAdvancedOpened={setAdvancedOpened}
-                    subscriptionTemplates={templates?.templates ?? []}
-                />
-            )}
+            {host && <EditHostModalContentWidget host={host} onClose={close} />}
         </Drawer>
     )
 })
